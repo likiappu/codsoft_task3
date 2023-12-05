9b76d9ca6449bd117a9a6063948f2790ae8e04de
likiappu <150883101+likiappu@users.noreply.github.com>
2023-12-05 13:17:24 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,18 @@
+var screen=document.getElementById('screen');
+
+function btnclick(value){
+    screen.value+=value;
+}
+
+function clearscreen(){
+    screen.value="";
+}
+
+function findresult(){
+    var result=eval(screen.value);
+    screen.value=result;
+}
+
+function deletelast(){
+    screen.value = screen.value.toString().slice(0,-1);
+}
